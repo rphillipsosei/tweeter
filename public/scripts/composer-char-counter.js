bafc7d19c08d07dfa748ff04567320baf9d8d980
Rhoda Phillips Osei <rhodaphillipsosei@gmail.com>
2022-02-01 22:18:06 +0000

    Implement character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#tweet-text').on('keyup', function() {
+   let newTweetLength = $(this).val().length;
+   let numsLeft = 140 - newTweetLength 
+   console.log("numsLeft", numsLeft)
+   console.log("newTweetLength", newTweetLength)
+   let charCounter = $(this).siblings('div').find('.counter').html(numsLeft);
+   if (numsLeft < 0) {
+     charCounter.addClass('tweetTooLong') && alert("Tweet too long");
+   } else if (numsLeft >= 0) {
+     charCounter.removeClass('tweetTooLong');
+    }
+ });
+ });
+
+ 
