ba7cb37ac484e4f6277fca725ae0cb4674c7dd94
Rhoda Phillips Osei <rhodaphillipsosei@gmail.com>
2022-02-02 23:50:58 +0000

    Set up AJAX request to load tweets without refresh, loadTweets get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,31 +6,7 @@
 
 $(document).ready(function () {
   // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
 
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
   const createTweetElement = function (tweetData) {
     const $toDynamic = `<article class="tweet">
 <header class="tweet-header">
@@ -70,4 +46,18 @@ $(document).ready(function () {
   };
 
   renderTweets(tweetData);
+
+  const $form = $("form");
+  $form.submit(function (event) {
+    event.preventDefault();
+    $.ajax("/tweets", { method: "POST" }).then(function () {
+      console.log($(this).serialize());
+    });
+  });
 });
+
+const loadTweets = function () {
+  $.get("/tweets").then((data) => renderTweets(data));
+};
+
+//selectors, variables, webdevsimplified
